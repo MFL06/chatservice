ae234f3837e2b84d0c08acecf5fbcb641462b1a1
MFL06 <mikkelljungbeck@gmail.com>
2025-10-06 16:00:32 +0200

    first function

--- API.py
+++ API.py
@@ -2,6 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+userList = []
+
+messageList = []
+
+class Users:
+    def __init__(self, name):
+        self.name = name
+    
+    def createUser(name):
+        userList.append(Users(name))
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     import uvicorn; from pathlib import Path
     uvicorn.run(f'{Path(__file__).resolve().stem}:app', host="0.0.0.0", port=8000, reload=True)
