9289293638da96a525df3a37c1cb59b151bf1c9c
MFL06 <mikkelljungbeck@gmail.com>
2025-10-08 14:14:37 +0200

    et eller andet

--- API.py
+++ API.py
@@ -1,17 +1,33 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
 userList = []
 
-messageList = []
+messages = [
+    {}
+]
+
+
 
 class Users:
-    def __init__(self, name):
+    def __init__(self, name, password):
         self.name = name
+        self.password = password
     
-    def createUser(name):
-        userList.append(Users(name))
+    async def ReceiveMsg(self, name, message: dict):
+        print('skibidi')
+
+@app.post('/messages')
+def createUser(name: str, password: str):
+    userList.append(Users(name, password))
+
+
+if __name__ == "__main__":
+    import uvicorn; from pathlib import Path
+    uvicorn.run(f'{Path(__file__).resolve().stem}:app', host="0.0.0.0", port=8000, reload=True)
+
 
 
 
