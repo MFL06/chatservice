74d2a79569330d6855d42e2ff481a1d638f2baa5
MFL06 <mikkelljungbeck@gmail.com>
2025-10-09 10:02:31 +0200

    summin

--- API.py
+++ API.py
@@ -27,20 +27,3 @@ def createUser(name: str, password: str):
 if __name__ == "__main__":
     import uvicorn; from pathlib import Path
     uvicorn.run(f'{Path(__file__).resolve().stem}:app', host="0.0.0.0", port=8000, reload=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    import uvicorn; from pathlib import Path
-    uvicorn.run(f'{Path(__file__).resolve().stem}:app', host="0.0.0.0", port=8000, reload=True)
