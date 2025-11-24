eb3073b36db1ef81bd6c392fd0d33fd918df9bd4
H3rsk <will.herskind@gmail.com>
2025-11-24 15:53:23 +0100

    gemmer brugerne

--- client.py
+++ client.py
@@ -2,6 +2,11 @@ import requests
 
 API_URL = "http://10.74.68.175:8000"
 
+class Bruger:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
 def opret_bruger():
     print("=== Opret ny bruger ===")
     name = input("name: ")
@@ -10,11 +15,19 @@ def opret_bruger():
     try:
         response = requests.post(f"{API_URL}/sign up", json={"name": name, "password": password})
         if response.status_code == 200:
-            print("Bruger oprettet!",response.text)
+            print("Bruger oprettet!", response.text)
+            return Bruger(name, password)
         else:
             print(f"Fejl: {response.status_code} - {response.text}")
+            return None
     except requests.exceptions.RequestException as e:
         print("Kunne ikke forbinde til serveren:", e)
+        return None
 
 if __name__ == "__main__":
-    opret_bruger()
+    bruger = opret_bruger()
+
+    if bruger:
+        print("Bruger objekt gemt:")
+        print("Navn:", bruger.name)
+        print("Password:", bruger.password)
